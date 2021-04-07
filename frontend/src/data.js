e8bda9f51033b59638c270df513e215e71159b52
Jobal <jbaldwije@gmail.com>
2021-04-06 23:28:43 -0400

    first

---
+++ frontend/src/data.js
@@ -0,0 +1,27 @@
+const data = {
+  products: [
+    {
+      _id:'1',
+      name:'Single Bag Subscription',
+      image:'/images/p1.png',
+      price: 16.95,
+      description: 'high quality product',
+    },
+    {
+      _id:'2',
+      name:'Double Bag Subscription',
+      image:'/images/p2.png',
+      price: 33.90,
+      description: 'high quality product',
+    },
+    {
+      _id:'3',
+      name:'Triple Bag Subscription',
+      image:'/images/p3.png',
+      price: 59.85,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
